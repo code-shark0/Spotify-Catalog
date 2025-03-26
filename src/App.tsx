34c7a8dc6704b87281fd277503c2a5ff1c77a39a
Eli Green <89555409+egreen0@users.noreply.github.com>
2025-03-25 20:19:04 -0700

    updated app with auth context and routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 import theme from './theme';
 import LoginPage from './pages/LoginPage';
+import { AuthProvider } from './context/AuthContext';
+import Homepage from "./pages/HomePage";
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-			<CssBaseline />
-			<LoginPage />
-		</ThemeProvider>
-  )
+    return (
+        <AuthProvider>
+            <ThemeProvider theme={theme}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/" element={<Homepage />} />
+                        <Route path="/callback" element={<LoginPage />} />
+                    </Routes>
+                </BrowserRouter>
+                <CssBaseline />
+                <LoginPage />
+            </ThemeProvider>
+        </AuthProvider>
+    )
 }
 
 export default App
